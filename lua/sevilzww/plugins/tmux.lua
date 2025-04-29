0bf93950d346fb5ae42b475492a65e3dca455fa0
Sevilze <sevilzcubing@gmail.com>
2025-04-29 23:47:12 +0700

    Fix conflicting vim-tmux navigation keybinds.

--- lua/sevilzww/plugins/tmux.lua
+++ lua/sevilzww/plugins/tmux.lua
@@ -3,16 +3,39 @@ return {
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
+    priority = 1000,
     config = function()
+      if vim.env.TMUX == nil or vim.env.TMUX == "" then
+        vim.env.TMUX = "force-tmux-mode"
+        vim.notify("Forced TMUX environment variable for vim-tmux-navigator", vim.log.levels.INFO)
+      end
+
       vim.g.tmux_navigator_no_mappings = 1
-      vim.keymap.set("n", "<C-h>", ":<C-U>TmuxNavigateLeft<CR>", { silent = true })
-      vim.keymap.set("n", "<C-j>", ":<C-U>TmuxNavigateDown<CR>", { silent = true })
-      vim.keymap.set("n", "<C-k>", ":<C-U>TmuxNavigateUp<CR>", { silent = true })
-      vim.keymap.set("n", "<C-l>", ":<C-U>TmuxNavigateRight<CR>", { silent = true })
-      vim.keymap.set("n", "<C-\\>", ":<C-U>TmuxNavigatePrevious<CR>", { silent = true })
+
+      -- Normal mode mappings with alt keys
+      vim.api.nvim_set_keymap('n', '<A-Left>', ':TmuxNavigateLeft<CR>', { silent = true, noremap = true })
+      vim.api.nvim_set_keymap('n', '<A-Down>', ':TmuxNavigateDown<CR>', { silent = true, noremap = true })
+      vim.api.nvim_set_keymap('n', '<A-Up>', ':TmuxNavigateUp<CR>', { silent = true, noremap = true })
+      vim.api.nvim_set_keymap('n', '<A-Right>', ':TmuxNavigateRight<CR>', { silent = true, noremap = true })
+
+      vim.api.nvim_set_keymap('n', '<A-h>', ':TmuxNavigateLeft<CR>', { silent = true, noremap = true })
+      vim.api.nvim_set_keymap('n', '<A-j>', ':TmuxNavigateDown<CR>', { silent = true, noremap = true })
+      vim.api.nvim_set_keymap('n', '<A-k>', ':TmuxNavigateUp<CR>', { silent = true, noremap = true })
+      vim.api.nvim_set_keymap('n', '<A-l>', ':TmuxNavigateRight<CR>', { silent = true, noremap = true })
+
+      -- Terminal mode mappings with alt keys
+      -- vim.api.nvim_set_keymap('t', '<A-Left>', '<C-\\><C-n>:TmuxNavigateLeft<CR>', { silent = true, noremap = true })
+      -- vim.api.nvim_set_keymap('t', '<A-Down>', '<C-\\><C-n>:TmuxNavigateDown<CR>', { silent = true, noremap = true })
+      -- vim.api.nvim_set_keymap('t', '<A-Up>', '<C-\\><C-n>:TmuxNavigateUp<CR>', { silent = true, noremap = true })
+      -- vim.api.nvim_set_keymap('t', '<A-Right>', '<C-\\><C-n>:TmuxNavigateRight<CR>', { silent = true, noremap = true })
+
+      -- vim.api.nvim_set_keymap('t', '<A-h>', '<C-\\><C-n>:TmuxNavigateLeft<CR>', { silent = true, noremap = true })
+      -- vim.api.nvim_set_keymap('t', '<A-j>', '<C-\\><C-n>:TmuxNavigateDown<CR>', { silent = true, noremap = true })
+      -- vim.api.nvim_set_keymap('t', '<A-k>', '<C-\\><C-n>:TmuxNavigateUp<CR>', { silent = true, noremap = true })
+      -- vim.api.nvim_set_keymap('t', '<A-l>', '<C-\\><C-n>:TmuxNavigateRight<CR>', { silent = true, noremap = true })
     end,
   },
-  
+
   {
     "preservim/vimux",
     lazy = true,
@@ -31,7 +54,7 @@ return {
       vim.g.VimuxHeight = "30"
       vim.g.VimuxOrientation = "h"
       vim.g.VimuxUseNearest = 1
-      
+
       vim.keymap.set("n", "<leader>tr", ":VimuxRunCommand<Space>", { desc = "tmux run command" })
       vim.keymap.set("n", "<leader>tl", ":VimuxRunLastCommand<CR>", { desc = "tmux run last command" })
       vim.keymap.set("n", "<leader>to", ":VimuxOpenRunner<CR>", { desc = "tmux open runner" })
@@ -41,19 +64,19 @@ return {
       vim.keymap.set("n", "<leader>tz", ":VimuxZoomRunner<CR>", { desc = "tmux zoom runner" })
     end,
   },
-  
+
   {
     "tmux-plugins/vim-tmux-focus-events",
     lazy = false,
     config = function()
       vim.o.autoread = true
-      
+
       -- Trigger autoread when files change on disk
       vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
         pattern = "*",
         command = "if mode() != 'c' | checktime | endif",
       })
-      
+
       -- Notification after file change
       vim.api.nvim_create_autocmd("FileChangedShellPost", {
         pattern = "*",
